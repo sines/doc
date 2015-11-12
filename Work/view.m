8f68091b3f8d00e6b96244c8948e5423d2627f22
liuhua <liuhua@misrobot.com>
2015-11-12 21:21:45 +0800

    view

--- Work/view.m
+++ Work/view.m
@@ -11,27 +11,29 @@ z=linspace(1,1,length);
 xlabel('vres');
 ylabel('hres');
 
-s=2;
+s=1;
 axis([0 hres*s 0 vres*s]);
 %set(gca,'xtick',[0:1:hres*s],'XColor','k');
 %set(gca,'ytick',[0:1:vres*s],'YColor','r');
 %figure;
 %% ray.o = Point3D(s * (x - hres / 2.0 + 0.5), s * (y - vres / 2.0 + 0.5), zw);s * (c - hres / 2.0 + 0.5), s * (r - vres / 2.0 + 0.5), zw)
-px = s*(x - hres / 2.0 + 0.5);
-py = s*(y - vres / 2.0 + 0.5);
+px = s*(x - hres / 2.0 + 0.005);
+py = s*(y - vres / 2.0 + 0.005);
 
 for n =(px)
     for i=(py)
-        stem(n, i); 
+        stem(n, i,'MarkerFaceColor','Green'); 
         hold on;
     end;
 end;
 
 a =(-5:1:5);
 b=linspace(5,5,length+1);
-stem(b, a,'MarkerFaceColor','red','LineStyle',':');
-stem(a, b,'MarkerFaceColor','red','LineStyle',':');
+stem(b, a,'LineStyle',':','Marker','square');
+stem(a, b,'LineStyle',':','Marker','square');
 b=linspace(-5,-5,length+1);
-stem(b, a,'MarkerFaceColor','red','LineStyle',':');
-stem(a, b,'MarkerFaceColor','red','LineStyle',':');
+stem(b, a,'LineStyle',':','Marker','square');
+stem(a, b,'LineStyle',':','Marker','square');
 grid on;
+
+
