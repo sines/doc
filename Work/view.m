c05d3d9c22c441b194cc081b45b8c56d05beed8b
sines <liuhua524@126.com>
2015-11-13 08:43:14 +0800

    Hammersley Sampler

--- Work/view.m
+++ Work/view.m
@@ -8,20 +8,29 @@ x =(0:1:vres-1);
 y =(0:1:hres-1);
 
 z=linspace(1,1,length);
-xlabel('vres');
-ylabel('hres');
 
+set(figure(1),'NumberTitle', 'off', 'Name', 'Hammersley Sampler');
 s=1;
 axis([0 hres*s 0 vres*s]);
 %set(gca,'xtick',[0:1:hres*s],'XColor','k');
 %set(gca,'ytick',[0:1:vres*s],'YColor','r');
 %figure;
 %% ray.o = Point3D(s * (x - hres / 2.0 + 0.5), s * (y - vres / 2.0 + 0.5), zw);s * (c - hres / 2.0 + 0.5), s * (r - vres / 2.0 + 0.5), zw)
-px = s*(x - hres / 2.0 + 0.005);
-py = s*(y - vres / 2.0 + 0.005);
+pnlength = 2;
 
+px = x - hres*0.5+0;
+py = y - vres*0.5+0;
 for n =(px)
     for i=(py)
+        for px_n = (1:pnlength)
+            sprintf('px_n= %f',px_n)
+            for py_n =(1:pnlength)
+                pn = n+0.5*(-1)^px_n;
+                pi = i+0.5*(-1)^py_n;
+                stem(pn, pi,'MarkerFaceColor','red'); 
+                hold on;
+            end;
+        end;
         stem(n, i,'MarkerFaceColor','Green'); 
         hold on;
     end;
@@ -35,5 +44,7 @@ b=linspace(-5,-5,length+1);
 stem(b, a,'LineStyle',':','Marker','square');
 stem(a, b,'LineStyle',':','Marker','square');
 grid on;
-
+title('Hammersley Sampler');
+xlabel('vres');
+ylabel('hres');
 
